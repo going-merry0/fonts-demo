7cf7e7bbe279c89da34b6112aeab977c33e44aa2
hsiaosiyuan <hsiaosiyuan0@outlook.com>
2018-04-15 21:23:49 +0800

    update

--- src/index.js
+++ src/index.js
@@ -69,4 +69,4 @@ app.get("/fonts", async (req, res) => {
 
 app.get("/*.ttf", express.static("../tmp"));
 
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
+app.listen(3535, () => console.log("Example app listening on port 3000!"));
